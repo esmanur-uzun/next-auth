dfd22eab52d24fce087fa225e6a3f80649f21106
seyirevleri <seyirevleri6@gmail.com>
2025-06-30 10:51:20 +0300

    Auth0 structure was established, user type was created and provider was defined.

---
+++ next-auth/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth/next";
+import Auth0Provider from "next-auth/providers/auth0";
+import type { Session } from "next-auth";
+
+
+const handler = NextAuth({
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      issuer: `https://${process.env.AUTH0_DOMAIN}`,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        token.id = profile.sub;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user = {
+          ...session.user,
+          id: token.id as string,
+        } as Session["user"];
+      }
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
