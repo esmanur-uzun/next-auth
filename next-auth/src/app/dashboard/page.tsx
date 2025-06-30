b13e9d99d0b3b6079124f9e2cb1ffb0cbf96fb27
seyirevleri <seyirevleri6@gmail.com>
2025-06-30 16:46:05 +0300

    created main page and dashboard page

---
+++ next-auth/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function DashboardPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="text-2xl">👋 Giriş yaptınız! Dashboard burası.</div>
+    </div>
+  );
+}
